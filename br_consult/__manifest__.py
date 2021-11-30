9679c99dbac451a5289f638d9759ae2709d2fc32
hedi.ad <hedi.adouni@gmail.com>
2021-11-30 13:37:50 +0000

    [INIT] Initialisation de module

---
+++ br_consult/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Br consult",
+
+    'summary': """
+        Nouveau module pour BR Consult
+        
+        """,
+
+
+    'author': "BR Consult",
+    'website': "http://www.brconsult.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Project',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+#         'views/views.xml',
+#         'views/templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
