6551e8744413379b3d8e77a9f05948a5c52e642c
hedi.ad <hedi.adouni@gmail.com>
2022-02-07 16:28:33 +0000

    [IMP] Differentier les reserves dans le tableau constat

--- br_consult/models/prestation_constat.py
+++ br_consult/models/prestation_constat.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class PrestationConstat(models.Model):
     _name = "prestation.constat"
     _description = "Prestation Constat"
 
-    name = fields.Char("Name")
+    name = fields.Char("Nom", store=True, compute='compute_constat_name')
     prestation_id = fields.Many2one('prestation.prestation', 'Prestation')
     type = fields.Selection(string="Type", related="verification_point_id.type", store=True)
     verification_point_id = fields.Many2one('prestation.verification.point', string="Point de vérification")
@@ -21,4 +21,37 @@ class PrestationConstat(models.Model):
         ('to_lift', "A lever"),
         ('lifted', "Levée")], string="Statut")
     date = fields.Date('Date')
+    
+
+    def action_show_details(self):
+        view = self.env.ref('br_consult.view_prestation_constat_form')
+        return {
+            'name': _('%s') % self.name,
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_model': 'form',
+            'res_model': 'prestation.constat',
+            'views': [(view.id, 'form')],
+            'view_id': view.id,
+            'target': 'current',
+            'res_id': self.id,
+            'context': dict(self.env.context),
+        }
+    
+    @api.depends('prestation_id', 'type', 'verification_point_id')
+    def compute_constat_name(self):
+        for line in self:
+            nom = ""
+            if line.prestation_id:
+                nom += line.prestation_id.name + " "
+            
+            if line.type:
+                nom += line.type + " "
+           
+            if line.verification_point_id:
+                nom += line.verification_point_id.name + " "
+            
+            line.update({'name': nom})
+            
+        
     
