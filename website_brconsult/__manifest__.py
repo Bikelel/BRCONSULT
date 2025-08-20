0213c7c525689f6d3294cf54bad8732300f41923
hedi.ad <hedi.adouni@gmail.com>
2025-08-20 13:23:47 +0000

    [FIX] Desactiver la localisation fr dans portal

--- website_brconsult/__manifest__.py
+++ website_brconsult/__manifest__.py
@@ -44,10 +44,10 @@
             'website_brconsult/static/js/portal_graph.js',
             'website_brconsult/static/js/portal_signature_brconsult.js',
         ],
-        'web._assets_common_scripts': [
-            'web/static/lib/moment/locale/fr.js',
-            'website_brconsult/static/src/js/date_portal.js',
-        ],
+        # 'web._assets_common_scripts': [
+        #     'web/static/lib/moment/locale/fr.js',
+        #     'website_brconsult/static/src/js/date_portal.js',
+        # ],
 
     },
 
